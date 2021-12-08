ef6d624c4e50bfb4dbf126518494e60696e416a6
BayronDavidMorales <93720978+BayronDavid@users.noreply.github.com>
2021-12-08 16:15:57 -0500

    Mircro servicio auth_ms completo y funcionando

--- src/resolvers/authResolver.js
+++ src/resolvers/authResolver.js
@@ -8,18 +8,36 @@ const userResolver = {
             else
                 return null;
         },
-        AllUserDetailById: async (_, {}, {dataSources, userIdToken}) => {
+        AllUserDetail: async (_, __, {dataSources}) => {
                 return await dataSources.authAPI.getAllUsers();
-        }
+        },
+
+        AllDriversDetail: async (_, __,{dataSources}) => {
+            return await dataSources.authAPI.getAllDrivers();
+        },
+        carByDriverId: async (_, {idDriver}, {dataSources}) => {
+            return await dataSources.authAPI.getCarByDriverID(idDriver);
+        },
+        AllCarsDetail: async (_, __, {dataSources}) => {
+            return await dataSources.authAPI.getAllCars();
+        },
     },
+
     //para post-delete-update
     Mutation: {
         signUpUser:async (_,{userInput},{dataSources})=>{
             const authInput = {
                 username: userInput.username,
                 password: userInput.password,
-                email   : userInput.email,
-                typeAccount: userInput.typeAccount
+                email: userInput.email,
+                name:userInput.name,
+                lastName: userInput.lastName,
+                birthDate: userInput.birthDate,
+                gender: userInput.gender,
+                documentNumber: userInput.documentNumber,
+                phoneNumber: userInput.phoneNumber,
+                typeAccount: userInput.typeAccount,
+                car: userInput.car
             }
             return await dataSources.authAPI.createUser(authInput);
         },
@@ -27,7 +45,8 @@ const userResolver = {
             return await dataSources.authAPI.authRequest(credentials);
  
         },
-        refreshToken:(_,{token},{dataSources})=> dataSources.authAPI.refreshToken(token)
+        refreshToken: (_, {refresh}, { dataSources }) => dataSources.authAPI.refreshToken(refresh),
+        
     }
 };
 
